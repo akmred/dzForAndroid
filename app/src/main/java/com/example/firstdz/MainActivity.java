1e85a53c487d31fb34246795f84778efc5930d1d
Korzhov_A <kora@1c.ru>
2020-02-19 13:55:13 +0300

    1.	В погодном приложении создайте еще один экран — экран для выбора города. На экране выбора города пользователь должен выбрать интересующий его город и по нажатию на кнопку переходить на уже сделанный вами экран отображения температуры. Как будут связаны и как будут взаимодействовать экраны выбора города и информации о температуре, мы узнаем из следующих занятий. Пока достаточно нарисовать экраны. 2.	Экран выбора города может включать EditText для ввода города, список наиболее популярных городов и CheckBox для дополнительной информации: скорость ветра и давление.

--- app/src/main/java/com/example/firstdz/MainActivity.java
+++ app/src/main/java/com/example/firstdz/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.firstdz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,35 +23,104 @@ public class MainActivity extends AppCompatActivity {
 
         setTextOnMainActivity();
 
-//        Button b_cuztomization = findViewById(R.id.customization);
-//        b_cuztomization.setText("Настройка");
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+        } else {
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()",
+                Toast.LENGTH_SHORT).show();
+
+        Log.d(LOG_TAG, "onCreate");
 
     }
 
-    public void setTextOnMainActivity() {
-        TextView town = findViewById(R.id.town);
-        town.setText(R.string.town);
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onStart");
 
-        TextView weather = findViewById(R.id.weather);
-        weather.setText("weather");
+        }
+
+        // восстановление
+        @Override
+        protected void onRestoreInstanceState(Bundle saveInstanceState){
+            super.onRestoreInstanceState(saveInstanceState);
+            Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onRestoreInstanceState");
+            saveInstanceState.putBoolean("");
+        }
+
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onResume");
+        }
+
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onPause");
+        }
+
+        // сохранение
+        @Override
+        protected void onSaveInstanceState(Bundle saveInstanceState){
+            super.onSaveInstanceState(saveInstanceState);
+            Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onSaveInstanceState");
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onStop");
+        }
+
+        @Override
+        protected void onRestart() {
+            super.onRestart();
+            Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onRestart");
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "onDestroy");
+        }
+
+
+        public void setTextOnMainActivity() {
+            TextView town = findViewById(R.id.town);
+            town.setText(R.string.town);
+
+            TextView weather = findViewById(R.id.weather);
+            weather.setText("weather");
 
     }
 
-    public void b_customization_onClick(View view){
-        Toast.makeText(this, "b_cusomization on click event!",
-        Toast.LENGTH_SHORT);
+        public void b_customization_onClick(View view){
+            Toast.makeText(this, "b_cusomization on click event!",
+            Toast.LENGTH_SHORT);
 
-        setContentView(R.layout.activity_customize);
-
-    }
-
-    public void backup_onClick(View view) {
-
-        setContentView(R.layout.activity_main);
-        setTextOnMainActivity();
+            setContentView(R.layout.activity_customize);
 
     }
 
+        public void backup_onClick(View view) {
 
+            setContentView(R.layout.activity_main);
+            setTextOnMainActivity();
+
+        }
 
 }
